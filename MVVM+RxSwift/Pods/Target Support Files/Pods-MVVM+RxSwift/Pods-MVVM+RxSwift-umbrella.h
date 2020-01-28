374d6e89d50eeabc4ad89f814fa278ae1373776a
jdm717 <jdm717@naver.com>
2020-01-28 21:40:10 +0900

    패키지명수정

--- MVVM+RxSwift/Pods/Target Support Files/Pods-MVVM+RxSwift/Pods-MVVM+RxSwift-umbrella.h
+++ MVVM+RxSwift/Pods/Target Support Files/Pods-MVVM+RxSwift/Pods-MVVM+RxSwift-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_MVVMVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_MVVMVersionString[];
+FOUNDATION_EXPORT double Pods_MVVM_RxSwiftVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_MVVM_RxSwiftVersionString[];
 
